ce7edcd2770edada77f05e82c367e506f9cac223
Paulius <baranauskas.paulius@gmail.com>
2020-05-24 21:30:20 +0300

    Allows to select cell values and creates Sudoku object with these values.

--- gui.py
+++ gui.py
@@ -1,22 +1,38 @@
 # %%
 import tkinter as tk
+from sudoku import Sudoku
+import numpy as np
 
 # %%
-class SudokuGui:
+class SudokuGui(Sudoku):
     def __init__(self, root):
-
+        empty_sudoku = [[None] * 9] * 9
+        self.sudoku = Sudoku(empty_sudoku)
         self.root = root
         self.mainframe = tk.Frame(self.root)
         self.draw_sudoku()
         self.draw_buttons()
         self.mainframe.pack(expand=True)
 
+    @staticmethod
+    def create_cell(sudoku_frame, row_id, col_id, value=""):
+        digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9", ""}
+        tkvar = tk.StringVar(root)
+        tkvar.set(value)
+        cell = tk.OptionMenu(sudoku_frame, tkvar, *digits)
+        cell.row_id = row_id
+        cell.col_id = col_id
+        cell.tkvar = tkvar
+        cell.grid(row=row_id, column=col_id)
+        return cell
+
     def draw_sudoku(self):
         self.sudoku_frame = tk.Frame(self.mainframe)
+        cells = []
         for row_id in range(9):
             for col_id in range(9):
-                cell = tk.Entry(self.sudoku_frame)
-                cell.grid(row=row_id, column=col_id)
+                cell = self.create_cell(self.sudoku_frame, row_id, col_id)
+                cells.append(cell)
         self.sudoku_frame.pack()
 
     def draw_buttons(self):
@@ -32,10 +48,34 @@ class SudokuGui:
         self.clear_button.pack()
 
     def submit_digits(self):
-        print("a")
+        matrix = self.loop_labels()
+        sudoku = Sudoku(matrix)
 
     def clear_digits(self):
-        print("b")
+        pass
+
+    @staticmethod
+    def insert_value(matrix, row_id, col_id, value):
+        if value == "":
+            value = None
+        else:
+            value = int(value)
+        matrix[row_id, col_id] = value
+        return matrix
+
+    @staticmethod
+    def get_empty_matrix():
+        matrix = [[None] * 9] * 9
+        matrix = np.array(matrix)
+        return matrix
+
+    def loop_labels(self):
+        matrix = self.get_empty_matrix()
+        for child in self.sudoku_frame.children.values():
+            matrix = self.insert_value(
+                matrix, child.row_id, child.col_id, child.tkvar.get()
+            )
+        return matrix
 
 
 #%%
