5a0e5e6105194f79616351605ba74dad79cb26be
Paulius <baranauskas.paulius@gmail.com>
2020-06-10 21:38:36 +0300

    Updated buttons placement. Added docstrings to code.

--- gui.py
+++ gui.py
@@ -16,10 +16,21 @@ class SudokuGui(Sudoku):
         self.mainframe.pack(expand=True)
 
     def create_cell(self, sudoku_frame, row_id, col_id, value):
-        digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9", ""}
+        """Creates a single cell for a single number.
+
+        Args:
+            sudoku_frame (Tk.Frame): Parent frame.
+            row_id (int): Row id of a sudoku.
+            col_id (int): Column id of a sudoku.
+            value (int): Number to be inserted to a cell.
+
+        Returns:
+            Tk.OptionMenu: Created dropdown list.
+        """
+        digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9", " "}
         tkvar = tk.StringVar(self.root)
         if value is None:
-            value = ""
+            value = " "
         else:
             value = str(value)
         tkvar.set(value)
@@ -31,6 +42,8 @@ class SudokuGui(Sudoku):
         return cell
 
     def draw_sudoku(self):
+        """Draws and fills cells for sudoku.
+        """
         self.sudoku_frame = tk.Frame(self.mainframe)
         cells = []
         for row_id, row in enumerate(self.sudoku.sudoku):
@@ -40,29 +53,47 @@ class SudokuGui(Sudoku):
         self.sudoku_frame.pack()
 
     def draw_buttons(self):
+        """Draws buttons frame and buttons. Also assigns commands to buttons.
+        """
         self.button_frame = tk.Frame(self.root)
         self.submit_button = tk.Button(
             self.button_frame, text="SUBMIT", command=self.submit_digits
         )
-        self.submit_button.pack()
         self.clear_button = tk.Button(
             self.button_frame, text="CLEAR", command=self.clear_digits
         )
+        self.submit_button.pack(side="left")
+        self.clear_button.pack(side="right")
         self.button_frame.pack()
-        self.clear_button.pack()
 
     def submit_digits(self):
+        """Runs through cells, takes digits from cells, passes rezults to a solver
+        and fills cells with solutions.
+        """
         matrix = self.loop_labels()
         self.sudoku.sudoku = force_sudoku(matrix)
         self.redraw_sudoku()
 
     def clear_digits(self):
+        """Clears all entered digits and redraws sudoku frame.
+        """
         self.sudoku.sudoku = self.sudoku.get_empty_matrix()
         self.redraw_sudoku()
 
     @staticmethod
     def insert_value(matrix, row_id, col_id, value):
-        if value == "":
+        """Parses input from a cell, inserts it into matrix and returns updated matrix.
+
+        Args:
+            matrix (np.matrix): Original matrix with sudoku values.
+            row_id (int): Id of row, where value should be inserted.
+            col_id (int): Id of column, where value should be inserted.
+            value (str): Value to be inserted to a matrix. It is converted to integer.
+
+        Returns:
+            np.matrix: Updated matrix.
+        """
+        if value == " ":
             value = None
         else:
             value = int(value)
@@ -71,6 +102,11 @@ class SudokuGui(Sudoku):
 
 
     def loop_labels(self):
+        """Loops through cells, extracts digits and formats matrix.
+
+        Returns:
+            np.matrix: Matrix filled with inserted values.
+        """
         matrix = self.get_empty_matrix()
         for child in self.sudoku_frame.children.values():
             matrix = self.insert_value(
@@ -79,5 +115,7 @@ class SudokuGui(Sudoku):
         return matrix
 
     def redraw_sudoku(self):
+        """Destroys sudoku frame and redraws new one.
+        """
         self.sudoku_frame.destroy()
         self.draw_sudoku()
